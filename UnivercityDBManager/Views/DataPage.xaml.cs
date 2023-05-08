1afb7b49f506c684195eb86b52ce5b5c241a40c8
Kos <turchakkonstantin@gmail.com>
2023-05-09 01:33:55 +0300

    added filters and animation for filtersField

--- UnivercityDBManager/Views/DataPage.xaml.cs
+++ UnivercityDBManager/Views/DataPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -113,11 +114,14 @@ namespace UnivercityDBManager.Views
                 if(studentsDataGrid.ItemsSource != null && searchField.Text != null)
                 {
                     Students student = studentsDataGrid.Items.Cast<Students>().FirstOrDefault(
-                        st => st.FirstName.Contains(searchField.Text)
-                        || st.LastName.Contains(searchField.Text));
+                        st => st.FirstName.ToLower().Contains(searchField.Text.ToLower())
+                        || st.LastName.ToLower().Contains(searchField.Text.ToLower()));
 
                     if (student != null)
+                    {
                         studentsDataGrid.ScrollIntoView(student);
+                        studentsDataGrid.SelectedItem = student;
+                    }
                     else
                         MessageBox.Show($"Студент содержащий \"{searchField.Text}\" в имени или фамилии не найден");
                 }                
@@ -127,11 +131,14 @@ namespace UnivercityDBManager.Views
                 if(coursesDataGrid.ItemsSource != null && searchField.Text != null)
                 {
                     Courses course = coursesDataGrid.Items.Cast<Courses>().FirstOrDefault(
-                        c => c.Name.Contains(searchField.Text) 
-                        || c.TeacherName.Contains(searchField.Text));
+                        c => c.Name.ToLower().Contains(searchField.Text.ToLower()) 
+                        || c.TeacherName.ToLower().Contains(searchField.Text.ToLower()));
 
                     if(course != null)
+                    {
                         coursesDataGrid.ScrollIntoView(course);
+                        coursesDataGrid.SelectedItem = course;
+                    }
                     else
                         MessageBox.Show($"Курс или преподаватель содержащий \"{searchField.Text}\" не найден");
                 }
@@ -141,12 +148,15 @@ namespace UnivercityDBManager.Views
                 if(relationshipDataGrid.ItemsSource == null && searchField.Text != null)
                 {
                     Relationship rel = relationshipDataGrid.Items.Cast<Relationship>().FirstOrDefault(
-                        r => r.CourseName.Contains(searchField.Text)
-                        || r.StudentFirstName.Contains(searchField.Text)
-                        || r.StudentLastName.Contains(searchField.Text));
+                        r => r.CourseName.ToLower().Contains(searchField.Text.ToLower())
+                        || r.StudentFirstName.ToLower().Contains(searchField.Text.ToLower())
+                        || r.StudentLastName.ToLower().Contains(searchField.Text.ToLower()));
 
                     if(rel != null)
+                    {
                         relationshipDataGrid.ScrollIntoView(rel);
+                        relationshipDataGrid.SelectedItem = rel;
+                    }                        
                     else
                         MessageBox.Show($"Фамилия или имя студента, название курса содержащие \"{searchField.Text}\" не найдены");
                 }
@@ -188,6 +198,92 @@ namespace UnivercityDBManager.Views
         public void RelationshipDataGridUpdate()
         {
             relationshipDataGrid.ItemsSource = RelationshipRepository.GetAllRelationships();
+        }
+
+        private void filterFieldStudents_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(filterFieldStudents.Text != null)
+            {
+                if(firstNameFilterStudents.IsChecked == true)
+                    studentsDataGrid.ItemsSource = from Students student in studentsDataGrid.ItemsSource
+                                               where student.FirstName.ToLower().Contains(filterFieldStudents.Text.ToLower())
+                                               select student;
+
+                else if(lastNameFilterStudents.IsChecked == true)
+                    studentsDataGrid.ItemsSource = from Students student in studentsDataGrid.ItemsSource
+                                                   where student.LastName.ToLower().Contains(filterFieldStudents.Text.ToLower())
+                                                   select student;
+
+                else if(ageFilterStudents.IsChecked == true)
+                    studentsDataGrid.ItemsSource = from Students student in studentsDataGrid.ItemsSource
+                                                   where student.Age.ToString().Contains(filterFieldStudents.Text)
+                                                   select student;
+            }            
+            else
+            {
+                studentsDataGrid.ItemsSource = StudentRepository.GetAllStudents();
+            }
+        }
+
+        private void isHowFilter_Checked(object sender, RoutedEventArgs e)
+        {
+            studentFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(0, 30, TimeSpan.FromSeconds(0.5)));
+            coursesFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(0, 30, TimeSpan.FromSeconds(0.5)));
+            relationshipFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(0, 30, TimeSpan.FromSeconds(0.5)));
+        }
+
+        private void isHowFilter_Unchecked(object sender, RoutedEventArgs e)
+        {
+            studentFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(30, 0, TimeSpan.FromSeconds(0.5)));
+            coursesFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(30, 0, TimeSpan.FromSeconds(0.5)));
+            relationshipFilterField.BeginAnimation(StackPanel.HeightProperty, new DoubleAnimation(30, 0, TimeSpan.FromSeconds(0.5)));
+            filterFieldStudents.Text = null;
+            filterFieldCourses.Text = null;
+            filterFieldRelationship.Text = null;
+        }
+
+        private void filterFieldCourses_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (filterFieldCourses.Text != null)
+            {
+                if (courceNameFilterCourses.IsChecked == true)
+                    coursesDataGrid.ItemsSource = from Courses cource in coursesDataGrid.ItemsSource
+                                                   where cource.Name.ToLower().Contains(filterFieldCourses.Text.ToLower())
+                                                   select cource;
+
+                else if (teacherFilterCourses.IsChecked == true)
+                    coursesDataGrid.ItemsSource = from Courses cource in coursesDataGrid.ItemsSource
+                                                   where cource.TeacherName.ToLower().Contains(filterFieldCourses.Text.ToLower())
+                                                   select cource;                
+            }
+            else
+            {
+                coursesDataGrid.ItemsSource = CoursesRepository.GetAllCourses();
+            }
+        }
+
+        private void filterFieldRelationship_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (filterFieldRelationship.Text != null)
+            {
+                if (courceNameFilterRelationship.IsChecked == true)
+                    relationshipDataGrid.ItemsSource = from Relationship rel in relationshipDataGrid.ItemsSource
+                                                  where rel.CourseName.ToLower().Contains(filterFieldRelationship.Text.ToLower())
+                                                  select rel;
+
+                else if (firstNameFilterRelationship.IsChecked == true)
+                    relationshipDataGrid.ItemsSource = from Relationship rel in relationshipDataGrid.ItemsSource
+                                                  where rel.StudentFirstName.ToLower().Contains(filterFieldRelationship.Text.ToLower())
+                                                  select rel;
+                else if (lastNameFilterRelationship.IsChecked == true)
+                    relationshipDataGrid.ItemsSource = from Relationship rel in relationshipDataGrid.ItemsSource
+                                                  where rel.StudentLastName.ToLower().Contains(filterFieldRelationship.Text.ToLower())
+                                                  select rel;
+            }
+            else
+            {
+                relationshipDataGrid.ItemsSource = RelationshipRepository.GetAllRelationships();
+            }
         }
     }
 }
